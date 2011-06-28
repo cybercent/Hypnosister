03fb36cf6f6bc977367ab3f831b41f36d5455dcd
Daniel Podaru <parisonrails@gmail.com>
2011-06-28 23:07:57 +0200

    rails scheleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hypnosister::Application.config.secret_token = 'd4e68ba63177e304905ee28782accd07a9466960a9a1be0c64bf701236cffa9d9998a59b53d80ead2a96520b6d12a5b706ab8d9f80c8214db01eca42f9051638'
